a30516c22df5a78765e31aa6274cb7e2308518d5
uzielroy <uziel.roy@gmail.com>
2016-11-28 18:08:20 +0200

    Added support for get a screen after login and changed framework

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,5 @@
+/*jslint browser:true, devel:true, white:true, vars:true */
+
 
 function validate(){
     var username = document.getElementById("login_username").value;
@@ -8,8 +10,7 @@ function validate(){
     return false;
     }
     else{
-        attempt --;// Decrementing by one.
-        alert("Loginn failed");
+        window.alert("Login failed");
 
     }
 }
@@ -17,19 +18,30 @@ function validate(){
 function validate2(){
     var xhr = new XMLHttpRequest();
     var username = document.getElementById("login_username").value;
-    var password = document.getElementById("login_password").value;
-      
-            xhr.onreadystatechange = function(){
-                  if(xhr.readyState == 4){
-                      
-                     var ajaxDisplay = document.getElementById('ajaxDiv');
-                     ajaxDisplay.innerHTML = xhr.responseText;
-                  }
-               }
-               //queryString +=  "&wpm=" + wpm + "&sex=" + sex;
-               xhr.open("GET", "http://localhost/smartprojects/server/api.php?action=is_user_exist&username=" + username.toString() +"&id=" +password.toString(), false);
-               xhr.send(username,password); 
+    var id = document.getElementById("login_password").value;
+    var json_response="";
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status==200 ){
+
+            var ajaxDisplay = document.getElementById('ajaxDiv');
+            json_response = xhr.responseText;
+            ajaxDisplay.innerHTML = json_response;
+        }
+    };
+    //queryString +=  "&wpm=" + wpm + "&sex=" + sex;
+    xhr.open("GET", "http://localhost/trunk/server/api.php?action=is_user_exist&username=" + username.toString() +"&id=" +id.toString(), false);
+    xhr.send(username,id);
+
+    var result = JSON.parse(json_response);
+    var id_res = eval('(' + result + ')');
+    
+    if (id_res.id == "1")
+    {
+        location.href="screen/third.html";
+    }
 }
+
 
 function validate3(){
     var xhr = new XMLHttpRequest();
@@ -37,12 +49,12 @@ function validate3(){
     var password = document.getElementById("login_password").value;
       
             xhr.onreadystatechange = function(){
-                  if(xhr.readyState == 4){
-                      
-                     var ajaxDisplay = document.getElementById('ajaxDiv');
-                     ajaxDisplay.innerHTML = xhr.responseText;
+                  if(xhr.readyState == 4 && xhr.status==200 ){
+                    
+                    var ajaxDisplay = document.getElementById('ajaxDiv');
+                    ajaxDisplay.innerHTML = xhr.responseText;
                   }
-               }
+               };
                //queryString +=  "&wpm=" + wpm + "&sex=" + sex;
                xhr.open("GET", "http://localhost/smartprojects/server/api.php?action=bgu_login&username=" + username.toString() +"&password=" +password.toString(), false);
                xhr.send(username,password); 
--- www/js/init-app.js
+++ www/js/init-app.js
@@ -3,32 +3,13 @@
  */
 
 
-// This file is a suggested initialization place for your code.
-// It is completely optional and not required.
-// It implements a Cordova "hide splashscreen" function, that may be useful.
-// Note the reference that includes it in the index.html file.
-
-
 /*jslint browser:true, devel:true, white:true, vars:true */
 /*global $:false, intel:false, app:false, dev:false */
 /*global myEventHandler:false, cordova:false, device:false */
 
 
 
-window.app = window.app || {} ;         // there should only be one of these...
-
-
-
-// Set to "true" if you want the console.log messages to appear.
-
-app.LOG = app.LOG || false ;
-
-app.consoleLog = function() {           // only emits console.log messages if app.LOG != false
-    if( app.LOG ) {
-        var args = Array.prototype.slice.call(arguments, 0) ;
-        console.log.apply(console, args) ;
-    }
-} ;
+window.app = window.app || {} ;         // there should only be one of these, but...
 
 
 
@@ -41,48 +22,48 @@ app.consoleLog = function() {           // only emits console.log messages if ap
 app.initEvents = function() {
     "use strict" ;
     var fName = "app.initEvents():" ;
-    app.consoleLog(fName, "entry") ;
+    console.log(fName, "entry") ;
 
-    // NOTE: initialize your third-party libraries and event handlers
+    // initialize third-party libraries and event handlers
 
     // initThirdPartyLibraryNumberOne() ;
     // initThirdPartyLibraryNumberTwo() ;
     // initThirdPartyLibraryNumberEtc() ;
 
-    // NOTE: initialize your application code
+    // initialize application code
 
     // initMyAppCodeNumberOne() ;
     // initMyAppCodeNumberTwo() ;
     // initMyAppCodeNumberEtc() ;
 
-    // NOTE: initialize your app event handlers, see app.js for a simple event handler example
+    // Initialize app event handlers.
+    // See app.js for a simple example.
 
-    // TODO: configure following to work with both touch and click events (mouse + touch)
-    // see http://msopentech.com/blog/2013/09/16/add-pinch-pointer-events-apache-cordova-phonegap-app/
-
-//...overly simple example...
+// ...overly simple example...
 //    var el, evt ;
 //
-//    if( navigator.msPointerEnabled || !('ontouchend' in window))    // if on Win 8 machine or no touch
-//        evt = "click" ;                                             // let touch become a click event
-//    else                                                            // else, assume touch events available
-//        evt = "touchend" ;                                          // not optimum, but works
+//    if( navigator.msPointerEnabled )                            // if on a Windows 8 machine
+//        evt = "click" ;                                         // let touch become a click event
+//    else                                                        // else, assume touch events available
+//        evt = "touchend" ;                                      // not the optimum solution...
 //
 //    el = document.getElementById("id_btnHello") ;
 //    el.addEventListener(evt, myEventHandler, false) ;
 
-    // NOTE: ...you can put other miscellaneous init stuff in this function...
-    // NOTE: ...and add whatever else you want to do now that the app has started...
-    // NOTE: ...or create your own init handlers outside of this file that trigger off the "app.Ready" event...
+    // after init is all done is a good time to remove our splash screen
 
-    app.initDebug() ;           // just for debug, not required; keep it if you want it or get rid of it
-    app.hideSplashScreen() ;    // after init is good time to remove splash screen; using a splash screen is optional
+    app.hideSplashScreen() ;                // a splash screen is optional for your app
+
+    // ...and whatever else you want to do now that the app has started...
+    // The following is just for debug, not required; keep it if you want or get rid of it.
+
+    // ...other miscellaneous init stuff here...
 
     // app initialization is done
     // app event handlers are ready
     // exit to idle state and wait for app events...
 
-    app.consoleLog(fName, "exit") ;
+    console.log(fName, "exit") ;
 } ;
 document.addEventListener("app.Ready", app.initEvents, false) ;
 
@@ -95,25 +76,26 @@ document.addEventListener("app.Ready", app.initEvents, false) ;
 app.initDebug = function() {
     "use strict" ;
     var fName = "app.initDebug():" ;
-    app.consoleLog(fName, "entry") ;
+    console.log(fName, "entry") ;
 
     if( window.device && device.cordova ) {                     // old Cordova 2.x version detection
-        app.consoleLog("device.version: " + device.cordova) ;   // print the cordova version string...
-        app.consoleLog("device.model: " + device.model) ;
-        app.consoleLog("device.platform: " + device.platform) ;
-        app.consoleLog("device.version: " + device.version) ;
+        console.log("device.version: " + device.cordova) ;      // print the cordova version string...
+        console.log("device.model: " + device.model) ;
+        console.log("device.platform: " + device.platform) ;
+        console.log("device.version: " + device.version) ;
     }
 
     if( window.cordova && cordova.version ) {                   // only works in Cordova 3.x
-        app.consoleLog("cordova.version: " + cordova.version) ; // print new Cordova 3.x version string...
+        console.log("cordova.version: " + cordova.version) ;    // print new Cordova 3.x version string...
 
         if( cordova.require ) {                                 // print included cordova plugins
-            app.consoleLog(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
+            console.log(JSON.stringify(cordova.require('cordova/plugin_list').metadata, null, 1)) ;
         }
     }
 
-    app.consoleLog(fName, "exit") ;
+    console.log(fName, "exit") ;
 } ;
+document.addEventListener("app.Ready", app.initDebug, false) ;
 
 
 
@@ -123,19 +105,14 @@ app.initDebug = function() {
 app.hideSplashScreen = function() {
     "use strict" ;
     var fName = "app.hideSplashScreen():" ;
-    app.consoleLog(fName, "entry") ;
+    console.log(fName, "entry") ;
 
-    // see https://github.com/01org/appframework/blob/master/documentation/detail/%24.ui.launch.md
-    // Do the following if you disabled App Framework autolaunch (in index.html, for example)
-    // $.ui.launch() ;
-
-    if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
+    if( navigator.splashscreen ) {                              // Cordova API detected
         navigator.splashscreen.hide() ;
     }
-    if( window.intel && intel.xdk && intel.xdk.device ) {           // Intel XDK device API detected, but...
-        if( intel.xdk.device.hideSplashScreen )                     // ...hideSplashScreen() is inside the base plugin
-            intel.xdk.device.hideSplashScreen() ;
+    if( window.intel && intel.xdk && intel.xdk.device ) {       // Intel XDK API detected
+        intel.xdk.device.hideSplashScreen() ;
     }
 
-    app.consoleLog(fName, "exit") ;
+    console.log(fName, "exit") ;
 } ;
